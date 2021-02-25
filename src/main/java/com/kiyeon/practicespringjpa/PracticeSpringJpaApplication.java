3ce779c71d93cdd7158fa3ab4adf85b933b7b1da
kiyeoneee <kyeoneee@gmail.com>
2021-02-26 01:31:39 +0900

    initial commit

---
+++ src/main/java/com/kiyeon/practicespringjpa/PracticeSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.kiyeon.practicespringjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeSpringJpaApplication.class, args);
+	}
+
+}
